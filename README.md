fa4442af508ed25ef99a10169c4db94a4b40b1f3
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-09-02 20:00:24 +0300

    adds copy and paste function with solving race condition problem

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
   - copying by only selecting text (DONE)
   - past by middle button in mouse (DONE)
   - needs to keep listening after running it only copies instance of running (DONE)
-  - solve race condition problem
+  - solve race condition problem (DONE)
+  - check for double left click for copying
+  - add release feature for middle click
--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 from pynput import mouse, keyboard
 import win32gui
-
+import time
 # initializing keyboard controller for pressing ctrlv and ctrlc
 keyboard_controller = keyboard.Controller()
 
 # copying function by pressing ctrlc shortcut
-def send_ctrl_c():
+def copying():
     try:
-        keyboard_controller.press(keyboard.Key.ctrl)
-        keyboard_controller.press('c')
-        keyboard_controller.release('c')
-        keyboard_controller.release(keyboard.Key.ctrl)
-            
+        with keyboard_controller.pressed(keyboard.Key.ctrl):
+            keyboard_controller.press('c')  
+            keyboard_controller.release('c')            
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def on_click(x, y, button, pressed):
-    # for copying from any window it recognizes the window that is on the top priority 
-    hwnd = win32gui.GetForegroundWindow()
-    win32gui.SetForegroundWindow(hwnd)
-    send_ctrl_c()
-    if button == mouse.Button.middle and pressed:
-        try:
-            keyboard_controller.press(keyboard.Key.ctrl)
+
+def pasting():
+    try:
+        with keyboard_controller.pressed(keyboard.Key.ctrl):
             keyboard_controller.press('v')
-            keyboard_controller.release('v')
-            keyboard_controller.release(keyboard.Key.ctrl)
-            
-        except Exception as e:
-            print(f"An error occurred: {e}")
+            keyboard_controller.release('v')            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        if button == mouse.Button.left: #needs to check double click here 
+            hwnd = win32gui.GetForegroundWindow()
+            win32gui.SetForegroundWindow(hwnd)
+            copying()
+            time.sleep(0.1)
+        elif button == mouse.Button.middle: #needs to release after clicking 
+            pasting()
+
 
 # Set up the mouse listener
 with mouse.Listener(on_click=on_click) as listener:
